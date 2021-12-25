1cb5f942f1fe1a4cbb22add51fc506e201056f0e
Christiaan Slabbert <21810354@sun.ac.za>
2021-12-26 00:22:45 +0200

    update animations

--- static/js/animation.js
+++ static/js/animation.js
@@ -13,6 +13,15 @@ sr.reveal (
     }
 );
 
+const sectionsArray = [
+    document.querySelector('#about'),
+    document.querySelector('#skills'),
+    document.querySelector('#portfolio'),
+    document.querySelector('#programming'),
+    document.querySelector('#info'),
+    document.querySelector('#contact')
+];
+
 const sr1 = ScrollReveal({
     origin:"top",
     distance:"-80px",
@@ -20,36 +29,33 @@ const sr1 = ScrollReveal({
     reset:false,
 });
 
-sr1.reveal (
-    `
-    .about,.skills,.portfolio
-    `,{
-        interval:300,
-    }
-);
+sr1.reveal(sectionsArray,{interval:300});
+
+const springArrayR = [
+    document.querySelector('#spring1'),
+    document.querySelector('#spring2'),
+    document.querySelector('#spring3'),
+    document.querySelector('#spring4')
+];
 
 const sr2 = ScrollReveal({
     origin:"right",
     distance:"50%",
-    duration:3000,
-    reset:false,
+    reset:false
 });
 
-sr2.reveal (
-    `
-    #spring1,#spring2
-    `
-);
+sr2.reveal(springArrayR);
+
+const springArrayL = [
+    document.querySelector('#spring1L'),
+    document.querySelector('#spring2L'),
+    document.querySelector('#spring3L')
+];
 
 const sr3 = ScrollReveal({
     origin:"left",
     distance:"50%",
-    duration:3000,
-    reset:false,
+    reset:false
 });
 
-sr3.reveal (
-    `
-    #spring1L,#spring2L
-    `
-);
+sr3.reveal(springArrayL);
