60575d20c1e7b06625e861c92a5811fe83cce377
Christiaan Slabbert <21810354@sun.ac.za>
2022-01-03 03:18:53 +0200

    update

--- static/js/functions.js
+++ static/js/functions.js
@@ -6,6 +6,11 @@ if (history.scrollRestoration) {
     }
 }
 
+function showBtn(id) {
+    var btn = "saveBtn" + id;
+    document.getElementById(btn).style.display = "block";
+}
+
 function search() {
     var input, filter, ul, li, div, form, i, txtValue;
     input = document.getElementById('myInput');
@@ -125,93 +130,6 @@ function change(id) {
                 }
             }
         }
-    }
-
-    var btn = "saveBtn" + id;
-
-    if (bool) {
-        document.getElementById(btn).style.display = "block";
-    } else {
-        document.getElementById(btn).style.display = "none";
-    }
-}
-
-function changeTech(id, type, confidence) {
-    var bool = false;
-    var input, ul, li, div, divs, form, label, i, j, k;
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName('li');
-
-    for (i = 0; i < li.length; i++) {
-        form = li[i].getElementsByTagName("form")[0];
-        divs =  form.getElementsByTagName("div");
-        for (j= 0; j < divs.length; j++) {
-            input = divs[j].getElementsByTagName("input")[0];
-            if ((input !== undefined) && (input.type !== "radio")) {
-                if (input.value.length > 0) {
-                    bool = true;
-                    break;
-                }
-            }
-        }
-    }
-
-    div =  form.getElementsByTagName("div")[1];
-    divs =  div.getElementsByTagName("div");
-    for (k = 0; k < divs.length; k++) {
-        label = divs[k].getElementsByTagName("label")[0];
-        input = label.getElementsByTagName("input")[0];
-        if (input.checked && (input.value !== type)) {
-            bool = true;
-            break;
-        }
-    }
-
-    div =  form.getElementsByTagName("div")[6];
-    divs =  div.getElementsByTagName("div");
-    for (k = 0; k < divs.length; k++) {
-        label = divs[k].getElementsByTagName("label")[0];
-        input = label.getElementsByTagName("input")[0];
-        if (input.checked && (input.value !== confidence)) {
-            bool = true;
-            break;
-        }
-    }
-
-    var btn = "saveBtn" + id;
-
-    if (bool) {
-        document.getElementById(btn).style.display = "block";
-    } else {
-        document.getElementById(btn).style.display = "none";
-    }
-}
-
-function changeProject(id) {
-    var bool = false;
-    var input, ul, li, divs, form, i, j;
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName('li');
-
-    for (i = 0; i < li.length; i++) {
-        form = li[i].getElementsByTagName("form")[0];
-        divs =  form.getElementsByTagName("div");
-        for (j= 0; j < divs.length; j++) {
-            input = divs[j].getElementsByTagName("input")[0];
-            if (input !== undefined) {
-                if (input.value.length > 0) {
-                    bool = true;
-                    break;
-                }
-            }
-        }
-    }
-
-    var txtAreaName = "updateDescription" + id;
-    var txtArea = document.getElementById(txtAreaName);
-
-    if (!bool && txtArea.value.length > 0) {
-        bool = true;
     }
 
     var btn = "saveBtn" + id;
